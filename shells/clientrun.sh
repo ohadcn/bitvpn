aad12e373140410d97ec6ffd75525d964cb87edb
Emanuel Bronshtein <e3amn2l@gmx.com>
2018-05-11 10:29:14 +0300

    Install curl

--- shells/clientrun.sh
+++ shells/clientrun.sh
@@ -1,3 +1,4 @@
+sudo apt-get install -y openvpn curl
 CURRENTIP=$(curl http://ipinfo.io/ip)
 echo "CURRENTIP = $CURRENTIP"
 cd ~
@@ -5,7 +6,6 @@ git clone https://github.com/ohadcn/BitVpn
 cd ~/BitVpn/pythons
 sed -i 's/ovpn_bin=.*/ovpn_bin=\/usr\/sbin\/openvpn/g' client_config.ini
 echo "Installing openvpn client"
-sudo apt-get install -y openvpn
 sudo python3 ninja_client.py &
 sleep 15
 NEWIP=$(curl http://ipinfo.io/ip)
