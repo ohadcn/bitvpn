5a64c7aca349dd31d03100732708c5175f038528
Ohad <ohadcn@gmail.com>
2018-06-04 16:11:32 +0300

    few bug fixes

--- pythons/ninja_client.py
+++ pythons/ninja_client.py
@@ -5,16 +5,18 @@ from json import loads as decode
 from uuid import uuid4 as rand_data
 from os import system
 from time import sleep
-from threading import Thread
+from threading import Thread, Event
 try:
     from ConfigParser import ConfigParser
 except:
     from configparser import ConfigParser
 
+ev = Event()
 is_running = True
 def constantly_pay(wallet, server_addr, myid):
     addr = server_addr + "/should_pay/" + myid
     print(addr)
+    all_paid = 0
     while is_running:
         try:
             res = get(addr)
@@ -23,10 +25,17 @@ def constantly_pay(wallet, server_addr, myid):
                 break
             req = res.text
             if req and len(req) > 10:
-                print(wallet.pay(req))
+                resp = wallet.pay(req)
+                try:
+                    res = decode(resp)
+                    c = int(res["result"]["msatoshi"])
+                    all_paid += c
+                    print("\r" + str(c) + "\t\ttotal paid: " + str(all_paid), end="")
+                except Exception as e:
+                    print("pay failed " + str(e))
         except Exception as e:
             print("failed to pay! " + addr + str(e))
-        sleep(10)
+        ev.wait(10)
 
 parser = OptionParser()
 parser.add_option("-c", "--config", dest="config_file",
@@ -40,15 +49,16 @@ config.read(options.config_file)
 
 wallet = remote_wallet(config["wallet"])
 ovpn_conf = None
+myid = str(rand_data())
 if options.server:
-    ovpn_conf = get(options.server).text
+    ovpn_conf = get(options.server + "/generateNewClient/" + myid).text
+    base_addr = options.server
 servers = decode(get(config['netninja']['server'] + "/get_servers/2").text)
 while not ovpn_conf:
     for server in servers:
         base_addr = "http://" + server
         addr = ""
         try:
-            myid = str(rand_data())
             addr = base_addr + "/generateNewClient/" + myid
             resp = get(addr)
             if resp.status_code != 200:
@@ -66,4 +76,5 @@ Thread(target=constantly_pay,args=(wallet,base_addr,myid,)).start()
 print("connecting")
 system('\"' + config['netninja']['ovpn_bin'] + "\" --config config.ovpn")
 is_running = False
+ev.set()
 exit(0)
